cd43012d0ed6ee011150863885c04767e9bcf18c
CadeDPercival <94868816+CadeDPercival@users.noreply.github.com>
2022-07-28 16:27:55 -0400

    Add files via upload

---
+++ CadePercival/CadePercival/js/script.js
@@ -0,0 +1,17 @@
+//Opens the hamburger menu
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+})
+
+//Closes the hamburger menu
+
+document.querySelectorAll(".nav-link").forEach(n => n.
+addEventListener("click", () => {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+}))
